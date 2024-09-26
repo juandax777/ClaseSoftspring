ee22e23b7b8ee9ed91b3742ac8b15f15cba40a33
juaun Name <juan@example.com>
2024-09-26 18:38:24 -0500

    (nuevo) primer comit de extructura

---
+++ src/main/java/co/ucentral/Gestionardor/GestionardorApplication.java
@@ -0,0 +1,13 @@
+package co.ucentral.Gestionardor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionardorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionardorApplication.class, args);
+	}
+
+}
